3b95891c2e8435986609cbf6ad957e0e15c286cd
Francesco Marano <francesco.mrn24@gmail.com>
2015-06-26 23:59:28 +0200

    Replaced use of importlib in requirements tests with more appropriate pkg_resource.require

--- test/test_requirements.py
+++ test/test_requirements.py
@@ -1,24 +1,22 @@
 import unittest
-import importlib
+
+import pkg_resources
+
 
 class TestRequirements(unittest.TestCase):
-    def test_colorama(self):
-        try:
-            colorama = importlib.import_module('colorama')
-        except ImportError:
-            colorama = None
+    def test_requirements(self):
+        # Check colorama requirement
+        pkg_resources.require('colorama')
 
-        self.assertIsNotNone(colorama, "Missing colorama mandatory requirement")
-
-    def test_pyquery(self):
-        try:
-            pyquery = importlib.import_module('pyquery')
-            # Missing __version__ in pyquery. Look at issue #107
-            # self.assertGreaterEqual(pyquery.__version__, '1.2.9', "PyQuery >= 1.2.9 required")
-        except ImportError:
-            pyquery = None
-
-        self.assertIsNotNone(pyquery, "Missing PyQuery mandatory requirement")
+        # Check pyquery requirement and version
+        pyquery_version = pkg_resources.require('pyquery')[0].version
+        major, minor, micro = pyquery_version.split('.')
+        self.assertTrue(
+            major > 1  # version 2 or greater
+            or (major == 1 and minor > 2)  # version 1.3 or greater
+            or (major == 1 and minor == 2 and micro >= 9),  # version 1.2.9 or greater
+            "PyQuery version MUST be >= 1.2.9 (got {version} instead)".format(version=pyquery_version)
+        )
 
 
 if __name__ == '__main__':
