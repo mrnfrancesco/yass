6183fc16413ffdff59e97e0158d524dee5f03348
Francesco Marano <francesco.marano@cy4gate.com>
2019-07-11 15:38:45 +0200

    Fix plugins meta to match new directives from related websites
    NOTE: Baidu seems to be unresponsive, Google blocks our requests.
          They will be eventually added again later.

--- yass/plugins.py
+++ yass/plugins.py
@@ -1,5 +1,5 @@
 # YASS, Yet Another Subdomainer Software
-# Copyright 2015 Francesco Marano (@mrnfrancesco) and individual contributors.
+# Copyright 2015-2019 Francesco Marano (@mrnfrancesco) and individual contributors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,49 +19,56 @@ from yass.models import PluginBase
 
 class Aol(PluginBase):
     class Meta:
-        search_url = 'http://search.aol.com/aol/search'
-        subdomains_selector = 'span[property="f:durl"]'
+        search_url = 'https://search.aol.com/aol/search'
+        subdomains_selector = 'span.fz-ms.fw-m.fc-12th.wr-bw.lh-17'
 
 
 class Ask(PluginBase):
     class Meta:
-        search_url = 'http://www.ask.com/web'
-        subdomains_selector = 'p.durl'
+        search_url = 'https://www.ask.com/web'
+        subdomains_selector = 'p.PartialSearchResults-item-url'
 
 
-class Baidu(PluginBase):
-    class Meta:
-        search_url = 'https://www.baidu.com/s'
-        query_param = 'wd'
-        subdomains_selector = 'span.g'
+# Website unresponsive
+#
+# class Baidu(PluginBase):
+#    class Meta:
+#        search_url = 'https://www.baidu.com/s'
+#        query_param = 'wd'
+#        subdomains_selector = 'span.g'
 
 
 class Bing(PluginBase):
     class Meta:
-        search_url = 'http://www.bing.com/search'
+        search_url = 'https://www.bing.com/search'
         subdomains_selector = '#b_results li.b_algo cite'
 
 
-class Google(PluginBase):
-    class Meta:
-        search_url = 'https://www.google.com/search'
-        subdomains_selector = 'li.g cite'
-        request_delay = 1
+# Anti-bot tricks make yass fails with Google
+#
+# class Google(PluginBase):
+#     class Meta:
+#         search_url = 'https://www.google.com/search'
+#         subdomains_selector = '#search div.g cite'
+#         request_delay = 1
 
 
 class StartPage(PluginBase):
     class Meta:
         search_url = 'https://startpage.com/do/search'
-        subdomains_selector = 'span.url'
+        subdomains_selector = 'span.search-item__url'
 
 
 class WebCrawler(PluginBase):
     class Meta:
-        search_url = 'http://www.webcrawler.com/search/web'
+        search_url = 'https://www.webcrawler.com/serp'
         subdomains_selector = 'div.resultDisplayUrl'
+        include_param = ''
+        exclude_param = '-'
+
 
 class Yahoo(PluginBase):
     class Meta:
         search_url = 'https://search.yahoo.com/search'
         query_param = 'p'
-        subdomains_selector = 'div.compTitle div span'
+        subdomains_selector = 'span.fz-ms.fw-m.fc-12th.wr-bw.lh-17'
