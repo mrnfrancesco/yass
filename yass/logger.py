29aab955ebc2e10d7dec40932e593f1329031284
Francesco Marano <francesco.marano@cy4gate.com>
2019-07-11 15:39:37 +0200

    Change copyrigth notice from 2015 to 2015-2019

--- yass/logger.py
+++ yass/logger.py
@@ -1,5 +1,5 @@
 # YASS, Yet Another Subdomainer Software
-# Copyright 2015 Francesco Marano (@mrnfrancesco) and individual contributors.
+# Copyright 2015-2019 Francesco Marano (@mrnfrancesco) and individual contributors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
